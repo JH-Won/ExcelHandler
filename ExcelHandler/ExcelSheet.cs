c36b34280577b9cc233156b1e858393a40566d7a
JH-Won <47111959+JH-Won@users.noreply.github.com>
2020-12-26 14:44:28 +0900

    예외 클래스 생성

--- ExcelHandler/ExcelSheet.cs
+++ ExcelHandler/ExcelSheet.cs
@@ -20,6 +20,12 @@ namespace ExcelHandler
         private Excel.Worksheet sheet;
         private Excel.Range range;
 
+        /// <summary>
+        /// 엑셀 파일과 시트 번호로 ExcelSheet 객체를 생성한다
+        /// 파일 경로 또는 시트 번호가 유효하지 않은 경우, 예외를 throw한다
+        /// </summary>
+        /// <param name="excelFile">full file path</param>
+        /// <param name="sheetNum">sheet number to apply</param>
         public ExcelSheet(string excelFile, int sheetNum = 1)
         {
             ValidatePath(excelFile);
@@ -35,6 +41,7 @@ namespace ExcelHandler
             range = sheet.UsedRange;
         }
 
+        // COM 오브젝트이므로 직접 리소스 해제한다
         ~ExcelSheet()
         {
             GC.Collect();
@@ -50,8 +57,17 @@ namespace ExcelHandler
             Marshal.ReleaseComObject(app);
         }
 
+        /// <summary>
+        /// 행 개수
+        /// </summary>
         public int RowsCount { get => range.Rows.Count; }
+        /// <summary>
+        /// 열 개수
+        /// </summary>
         public int ColumnsCount { get => range.Columns.Count; }
+        /// <summary>
+        /// 유효 데이터 셀 개수
+        /// </summary>
         public int DataCellCount
         {
             get
@@ -65,6 +81,13 @@ namespace ExcelHandler
             }
         }
 
+        /// <summary>
+        /// 셀 데이터를 읽거나 쓴다
+        /// 인덱스는 1부터 시작하고, 범위 초과시 예외를 throw한다
+        /// </summary>
+        /// <param name="row">row index, 1부터 시작</param>
+        /// <param name="column">column index, 1부터 시작</param>
+        /// <returns></returns>
         public object this[int row, int column]
         {
             get
@@ -93,6 +116,11 @@ namespace ExcelHandler
             }
         }
 
+        /// <summary>
+        /// 해당 시트의 모든 데이터를 DataTable로 만들어 반환한다
+        /// 데이터 타입은 모두 Object타입으로 반환한다
+        /// </summary>
+        /// <returns>반환된 데이터테이블</returns>
         public DataTable GetFullDataTable()
         {
             DataTable ret = new DataTable();
@@ -103,6 +131,11 @@ namespace ExcelHandler
             return ret;
         }
 
+        /// <summary>
+        /// 첫번째 열을 컬럼 이름으로 하는 컬럼 이름 리스트를 반환한다
+        /// </summary>
+        /// <param name="isHeaderColumn">true if header is column names</param>
+        /// <returns></returns>
         public IList<string> GetColumnNames(bool isHeaderColumn = true)
         {
             List<string> ret = new List<string>();
@@ -116,18 +149,27 @@ namespace ExcelHandler
             return ret;            
         }
 
+        /// <summary>
+        /// 현재 열어둔 ExcelSheet의 변경사항을 저장한다
+        /// </summary>
         public void Save()
         {
             this.book.Save();
         }
 
+        /// <summary>
+        /// 현재 시트의 복사본을 새 파일 경로에 저장한다
+        /// 경로가 유효하지 않은 경우, 예외를 throw하고 저장되지 않는다
+        /// </summary>
+        /// <param name="filePath">저장할 파일 경로</param>
         public void SaveAs(string filePath)
         {
             ValidatePath(filePath);
 
-            this.book.SaveAs(filePath);
+            this.book.SaveCopyAs(filePath);
         }
 
+        #region private methods
         private void ValidatePath(string filePath)
         {
             if (filePath == originFilePath)
@@ -156,5 +198,6 @@ namespace ExcelHandler
             else if (column < 1 || column > ColumnsCount)
                 throw new IndexOutOfRangeException("column index out of range, excel sheet's cell index starts with 1(is not zero-based) and ends with row or coulmn count");
         }
+        #endregion
     }
 }
